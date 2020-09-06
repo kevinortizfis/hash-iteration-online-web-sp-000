d8319be876630edc12745c5afc98ed0141457bdc
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-09-06 22:17:21 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,6 +6,6 @@
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kid, age|
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    puts "Happy Birthday #{kid}! You are now #{age} years old!"
 end
 end
