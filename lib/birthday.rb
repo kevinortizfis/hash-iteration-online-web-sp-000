a3521a7a9dea5dc2272be9314a662a90d2253803
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-09-06 22:19:27 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -14,7 +14,7 @@ def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kid, age|
     if age <= 12
     puts "Happy Birthday #{kid}! You are now #{age} years old!"
-  else puts "You are too old for this"
+  else puts "You are too old for this."
   end
 end
 end
